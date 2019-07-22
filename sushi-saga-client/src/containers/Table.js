42ad04ce156d064ce2d2f3e4e31b63dc5a1dabac
mbarrins <maija.barrins@gmail.com>
2019-07-22 12:35:32 +0100

    changed Table back to functional component with useState hooks

--- sushi-saga-client/src/containers/Table.js
+++ sushi-saga-client/src/containers/Table.js
@@ -1,61 +1,56 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 import Button from '../components/Button'
 import SushiWallet from '../components/SushiWallet'
 
-class Table extends React.Component {
-  state = {
-    showForm: false,
-    addAmount: 0
-  }
+const Table = ({eaten, money, addMoney}) => {
+  const [showForm, setShowForm] = useState(false);
+  const [addAmount, setAddAmount] = useState(0);
 
-  renderPlates = (array) => {
+  const renderPlates = (array) => {
     return array.map((x, index) => {
       return <div className="empty-plate" key={index} style={{ top: -7 * index }}/>
     })
   }
 
-  toggleForm = () => {
-    this.setState({showForm: !this.state.showForm, addAmount: 0})
+  const toggleForm = () => {
+    setShowForm(!showForm);
+    setAddAmount(0);
   }
 
-  updateAmount = (event) => {
-    this.setState({addAmount: parseInt(event.target.value, 10)});
+  const updateAmount = (event) => {
+    setAddAmount(parseInt(event.target.value, 10));
   }
 
-  handleClick = event => {
+  const handleClick = event => {
     event.preventDefault();
-    this.props.addMoney(this.state.addAmount);
-    this.setState({showForm: false, addAmount: 0})
+    addMoney(addAmount);
+    toggleForm();
   }
 
-  render() {
-    const {eaten, money} = this.props
-
-    return (
-      <Fragment>
-        <h1 className="remaining">
-          You have: ${ money } remaining!
-          {this.state.showForm ? 
-              <SushiWallet addAmount={this.state.addAmount} handleClick={this.handleClick} toggleForm={this.toggleForm} updateAmount={this.updateAmount}/>
-            : 
-              <Button handleClick={this.toggleForm} text='Add Money'/>
-            }
-        </h1>
-        <div className="table">
-          <div className="stack">
-            {
-              /* 
-                renderPlates takes an array 
-                and renders an empty plate
-                for every element in the array
-              */
-              this.renderPlates(eaten)
-            }
-          </div>
+  return (
+    <Fragment>
+      <h1 className="remaining">
+        You have: ${ money } remaining!
+        {showForm ? 
+            <SushiWallet addAmount={addAmount} handleClick={handleClick} toggleForm={toggleForm} updateAmount={updateAmount}/>
+          : 
+            <Button handleClick={toggleForm} text='Add Money'/>
+          }
+      </h1>
+      <div className="table">
+        <div className="stack">
+          {
+            /* 
+              renderPlates takes an array 
+              and renders an empty plate
+              for every element in the array
+            */
+            renderPlates(eaten)
+          }
         </div>
-      </Fragment>
-    )
-  }
+      </div>
+    </Fragment>
+  )
 }
 
 export default Table
